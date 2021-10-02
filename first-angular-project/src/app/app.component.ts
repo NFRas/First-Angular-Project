03669d6c7d65bcc9d63f16ca6acb0103f604d56e
NFRas <nrasoulipour@gmail.com>
2021-10-02 18:07:08 +0330

    practice codes

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+@Component ({
+  selector: 'app-component-overview',
+  templateUrl: './component-overview.component.html', 
+  styleUrls: ['./component-overview.component.css'],
 })
-export class AppComponent {
-  title = 'first-angular-project';
+
+export class componentOverviewComponent {
+
+}
+
+//components with manual template and style (instead of inserting sheets)
+
+import {  mycomponent } from '@angular/core';
+
+@mycomponent ({
+  selector: 'yellow-world',
+  template: `<h1> Header goes like this </h1>
+            <p> and the paragraph goes like this</p>`,
+  styles: [ `h1 {font-weight: normal; }`]
+})
+
+export class yellowWorldComponent {
+  
 }
