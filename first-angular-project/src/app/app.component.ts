0aba733cf137daf09f592617d40db13bfb3263da
NFRas <nrasoulipour@gmail.com>
2021-10-06 14:30:12 +0330

    working on the angular project

--- first-angular-project/src/app/app.component.ts
+++ first-angular-project/src/app/app.component.ts
@@ -1,26 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component ({
-  selector: 'app-component-overview',
-  templateUrl: './component-overview.component.html', 
-  styleUrls: ['./component-overview.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.scss'],
 })
 
-export class componentOverviewComponent {
-
-}
-
-//components with manual template and style (instead of inserting sheets)
-
-import {  mycomponent } from '@angular/core';
-
-@mycomponent ({
-  selector: 'yellow-world',
-  template: `<h1> Header goes like this </h1>
-            <p> and the paragraph goes like this</p>`,
-  styles: [ `h1 {font-weight: normal; }`]
-})
-
-export class yellowWorldComponent {
-  
+export class AppComponent {
+  title = 'test'
 }
