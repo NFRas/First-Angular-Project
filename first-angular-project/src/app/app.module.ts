9e46922c4f3dd2d1e0665e711bc825439364ddbe
NFRas <nrasoulipour@gmail.com>
2021-10-13 14:23:35 +0330

    data bindings and formcontrols

--- first-angular-project/src/app/app.module.ts
+++ first-angular-project/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
     LoginComponent,
+    NameEditorComponent,
   ],
   imports: [
     FormsModule,
