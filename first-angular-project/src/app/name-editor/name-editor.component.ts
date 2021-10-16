36c5fc2fc2baa112c1700842b3c02e35f2e112bc
NFRas <nrasoulipour@gmail.com>
2021-10-16 12:32:30 +0330

    trying to make formcontrol

--- first-angular-project/src/app/name-editor/name-editor.component.ts
+++ first-angular-project/src/app/name-editor/name-editor.component.ts
@@ -1,25 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormControl, FormGroup } from '@angular/forms';
-
 @Component({
   selector: 'app-name-editor',
   templateUrl: './name-editor.component.html',
   styleUrls: ['./name-editor.component.scss']
 })
 export class NameEditorComponent implements OnInit {
-  accountInfo!: FormGroup;
 
   constructor() { }
 
-  ngOnInit(): void {
-     this.accountInfo = new FormGroup({
-       'emailName' : new FormControl(),
-       'thePassword' : new FormControl()
-     })
-  }
-
-  showInputs () {
-    console.log(this.accountInfo.value);
-  }
+  ngOnInit(){}
+  
 }
